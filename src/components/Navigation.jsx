c8e16d51ef67ce424632662c112e52cff46f7365
Ra <132078939+RPRd001@users.noreply.github.com>
2024-06-10 12:40:49 -0400

    Finished navigation styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,24 @@
 import logo from "../images/logo.svg"
 
 const Navigation = () => {
+
+    const linkText = ["Home", "About", "Contact", "Blog", "Careers"]
+
+    const linkStyles = 'flex items-center border-b-4 border-transparent hover:border-b-4 hover:border-[#30cd74] h-[100%] hover:cursor-pointer hover:text-[#3f424c]'
+
 	return (
-		<header className="flex flex-row items-center justify-around py-5 fixed top-0 left-0 w-full bg-white">
+		<header className="flex flex-row items-center justify-around h-[60px] max-h-[60px] fixed top-0 left-0 w-full bg-white">
 			<img src={logo} alt="easybank logo" />
 
-			<nav>
-				<ul className="flex flex-row items-center gap-4 text-[#b3b2b8]">
-					<li>Home</li>
-					<li>About</li>
-					<li>Contact</li>
-					<li>Blog</li>
-					<li>Careers</li>
+			<nav className="h-full">
+				<ul className="flex flex-row items-center gap-4 text-[#b3b2b8] h-full">
+					{linkText.map((link, i) => {
+                        return (<li key={i} className={linkStyles}>{link}</li>)
+                    })}
 				</ul>
 			</nav>
 
-			<button className="px-7 py-3 font-bold bg-gradient-to-r from-[#30ce69] to-[#2bb8c8] rounded-full text-white text-sm">Request Invite</button>
+			<button className="px-7 py-3 font-bold bg-gradient-to-r from-[#30ce69] to-[#2bb8c8] rounded-full text-white text-sm hover:opacity-60">Request Invite</button>
 		</header>
 	);
 };
